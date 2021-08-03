9d9333c9bdd24be2216adb3e6475cd5892856e66
Subin Yun <bigdatasubin@gmail.com>
2021-08-03 14:11:09 +0900

    java-회원가입

---
+++ pro17/src/sec02/ex02/MemberDAO.java
@@ -0,0 +1,122 @@
+package sec02.ex02;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import sec02.ex02.MemberVO;
+
+public class MemberDAO {
+	private DataSource dataFactory;
+	private Connection conn;
+	private PreparedStatement pstmt;
+
+	public MemberDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<MemberVO> listMembers() {
+		List<MemberVO>  membersList = new ArrayList();
+		try {
+			conn = dataFactory.getConnection();
+			String query = "select * from  t_member order by joinDate desc";
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				MemberVO memberVO = new MemberVO(id, pwd, name, email, joinDate);
+				membersList.add(memberVO);
+			}
+			rs.close();
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return membersList;
+	}
+	
+	// db삽입
+	public void addMember(MemberVO m) {
+		try {
+			conn = dataFactory.getConnection();
+			String id = m.getId();
+			String pwd = m.getPwd();
+			String name = m.getName();
+			String email = m.getEmail();
+			String query = "INSERT INTO t_member(id, pwd, name, email)" + " VALUES(?, ? ,? ,?)";
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			pstmt.setString(3, name);
+			pstmt.setString(4, email);
+			pstmt.executeUpdate();
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 해당 id가 데이터베이스 안에 있는지 조회
+	public MemberVO findMemeber(String _id) {
+		MemberVO memInfo = null;
+		try {
+			conn = dataFactory.getConnection();
+			String query = "select * from t_member where  id=?"; 
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, _id);
+			System.out.println(query);
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			String id = rs.getString("id");
+			String pwd = rs.getString("pwd");
+			String name = rs.getString("name");
+			String email = rs.getString("email");
+			Date joinDate = rs.getDate("joinDate");
+
+			memInfo = new MemberVO(id, pwd, name, email, joinDate);
+			pstmt.close();
+			conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return memInfo; // null이라면 해당 id가 데이터베이스에 없다는 뜻 
+	}
+
+	// 클라이언트로부터 수정하는 자료를 입력받아서  수정
+	public void modMember(MemberVO memberVO) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// 클라이언트로부터 삭제하는 id를 입력받아서 삭제
+	public void delMember(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
